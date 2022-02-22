b07fc85e49fe1797f2f6a9783e59f44f6b5382ca
SvetaZhadan <sveta.zhadan12@mail.ru>
2022-02-22 15:22:15 +0300

    footer, swiper

--- app/imports.js
+++ app/imports.js
@@ -2,7 +2,6 @@
 import './scss/index.scss';
 //js
 import $ from 'jquery';
-import Swiper from 'swiper';
 
 import { Fancybox } from "@fancyapps/ui";
 import IMask from 'imask';
--- app/js/index.js
+++ app/js/index.js
@@ -1,25 +1,25 @@
-import Swiper from 'swiper';
-import 'swiper/css/bundle';
+import { Swiper, Navigation, Pagination, Scrollbar } from 'swiper';
+Swiper.use([Navigation, Pagination, Scrollbar]);
+import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
+  modules: [Navigation, Pagination],
+  
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
 });
