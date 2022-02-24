8995840da93cbf59c7a0ae2052088ac0d5b11502
SvetaZhadan <sveta.zhadan12@mail.ru>
2022-02-24 15:50:08 +0300

    media

--- app/imports.js
+++ app/imports.js
@@ -2,12 +2,7 @@
 import './scss/index.scss';
 //js
 import $ from 'jquery';
-
-import { Fancybox } from "@fancyapps/ui";
-import IMask from 'imask';
 import 'validate.js';
 import 'tabbyjs';
-import ApexCharts from 'apexcharts';
-import scrolly from 'scrolly';
 import './js/ripple.js';
 import './js/index.js';
--- app/js/index.js
+++ app/js/index.js
@@ -23,3 +23,20 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+$(document).ready(function() {
+  $(".menuToggle").click(function() {
+    $(this).toggleClass("active");
+    $('.m-mobileNav').slideToggle(0,function(){
+      $('.m-col-center').css("display","none");
+      $('.hamburger.hidden').css("display","block")
+      $('.hamburger.active').css("display","none")
+      if($(this).css('display') === "none"){
+        $(this).removeAttr('style');
+        $('.m-col-center').css("display","block")
+        $('.hamburger.hidden').css("display","none")
+        $('.hamburger.active').css("display","block")
+      }
+    });
+  });
+});
