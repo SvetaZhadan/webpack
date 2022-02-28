7dbdab4349a0eca7415ec4c7c9f406414c613bf1
SvetaZhadan <sveta.zhadan12@mail.ru>
2022-02-28 15:45:53 +0300

    search input

--- app/js/index.js
+++ app/js/index.js
@@ -27,26 +27,38 @@ const swiper = new Swiper('.swiper', {
 
 $(document).ready(function() {
   $(".menuToggle").click(function() {
+ 
     $(this).toggleClass("active");
     $('.m-mobileNav').slideToggle(0,function(){
-      // $('.m-col-center').css("display","none");
       $('.hamburger.hidden').css("display","block");
       $('.hamburger.active').css("display","none");
       $('.a-btnLang').css("display","none");
       $('.a-btnLang.absolute').css("display","none");
       $('.a-phoneNumber').css("display","flex");
       $('.copyright').css("display","block");
-    
+      document.body.style.overflow = "hidden";
       if($(this).css('display') === "none"){
         $(this).removeAttr('style');
         $('.m-col-center').css("display","block");
         $('.hamburger.hidden').css("display","none");
         $('.hamburger.active').css("display","block");
-        // $('.a-btnLang').css("display","block");
         $('.a-btnLang.absolute').css("display","none");
         $('.a-phoneNumber').css("display","none");
         $('.copyright').css("display","none");
+        document.body.style.overflow = "";
       }
+    });
+  });
+});
+
+$(document).ready(function() {
+  $(".a-closeSerchPopup").click(function() {
+    $('.m-serchPopup').slideToggle(0,function(){
+    });
+  });
+  $(".btnSearch").click(function(){
+    $('.m-serchPopup').slideToggle(0,function(){
+      $('.m-serchPopup').css("display","flex");
     });
   });
 });
