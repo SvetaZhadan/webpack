8923f11f74c1b8e28bc39c0748e3c8cbed0decfe
SvetaZhadan <83220461+SvetaZhadan@users.noreply.github.com>
2022-03-01 16:10:00 +0300

    call back

--- app/imports.js
+++ app/imports.js
@@ -2,8 +2,12 @@
 import './scss/index.scss';
 //js
 import $ from 'jquery';
+import Swiper from 'swiper';
+import { Fancybox } from "@fancyapps/ui";
+import IMask from 'imask';
 import 'validate.js';
 import 'tabbyjs';
+import ApexCharts from 'apexcharts';
+import scrolly from 'scrolly';
 import './js/ripple.js';
-// import './js/swiper.js';
 import './js/index.js';
--- app/js/index.js
+++ app/js/index.js
@@ -6,7 +6,7 @@ import 'swiper/css/pagination';
 
 const swiper = new Swiper('.swiper', {
   modules: [Navigation, Pagination],
-  
+
   direction: 'horizontal',
   loop: true,
   pagination: {
@@ -24,40 +24,53 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-$(document).ready(function() {
-  $(".menuToggle").click(function() {
- 
+$(document).ready(function () {
+  $(".menuToggle").click(function () {
+
     $(this).toggleClass("active");
-    $('.m-mobileNav').slideToggle(0,function(){
-      $('.hamburger.hidden').css("display","block");
-      $('.hamburger.active').css("display","none");
-      $('.a-btnLang').css("display","none");
-      $('.a-btnLang.absolute').css("display","none");
-      $('.a-phoneNumber').css("display","flex");
-      $('.copyright').css("display","block");
-      document.body.style.overflow = "hidden";
-      if($(this).css('display') === "none"){
+    $('.m-mobileNav').slideToggle(0, function () {
+      $('.hamburger.hidden').css("display", "block");
+      $('.hamburger.active').css("display", "none");
+      $('.a-btnLang').css("display", "none");
+      $('.a-btnLang.absolute').css("display", "none");
+      $('.a-phoneNumber').css("display", "flex");
+      $('.copyright').css("display", "block");
+      // document.body.style.overflow = "hidden";
+      if ($(this).css('display') === "none") {
         $(this).removeAttr('style');
-        $('.m-col-center').css("display","block");
-        $('.hamburger.hidden').css("display","none");
-        $('.hamburger.active').css("display","block");
-        $('.a-btnLang.absolute').css("display","none");
-        $('.a-phoneNumber').css("display","none");
-        $('.copyright').css("display","none");
-        document.body.style.overflow = "";
+        $('.m-col-center').css("display", "block");
+        $('.hamburger.hidden').css("display", "none");
+        $('.hamburger.active').css("display", "block");
+        $('.a-btnLang.absolute').css("display", "none");
+        $('.a-phoneNumber').css("display", "none");
+        $('.copyright').css("display", "none");
+        // document.body.style.overflow = "";
       }
+    });
+  });
+
+  $(".a-closeSerchPopup").click(function () {
+    $('.m-serchPopup').slideToggle(0, function () {
+    });
+  });
+  $(".btnSearch").click(function () {
+    $('.m-serchPopup').slideToggle(0, function () {
+      $('.m-serchPopup').css("display", "flex");
+    });
+  });
+
+  $(".btnCallBack").click(function () {
+    $('.t-popup').slideToggle(0, function () {
+      $('.t-popup').css("display", "flex");
+      $('.m-popupBackCall').css("display", "flex");
+      document.body.style.overflow = "hidden";
+    });
+  });
+  $(".CloseCallBack").click(function () {
+    $('.t-popup').slideToggle(0, function () {
+      $('.t-popup').css("display", "none");
+      document.body.style.overflow = "";
     });
   });
 });
 
-$(document).ready(function() {
-  $(".a-closeSerchPopup").click(function() {
-    $('.m-serchPopup').slideToggle(0,function(){
-    });
-  });
-  $(".btnSearch").click(function(){
-    $('.m-serchPopup').slideToggle(0,function(){
-      $('.m-serchPopup').css("display","flex");
-    });
-  });
-});
